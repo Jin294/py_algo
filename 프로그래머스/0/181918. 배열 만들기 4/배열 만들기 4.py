8515f7eb0736706bf499ebbbcddecfd0eb2c808d
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2025-09-24 09:30:33 +0900

    [level 0] Title: 배열 만들기 4, Time: 50.51 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -1,10 +1,8 @@
 def solution(arr):
     stk = []
-    
     i = 0
     while i < len(arr):
-        stk_size = len(stk)
-        if stk_size > 0 and stk[stk_size - 1] >= arr[i]:
+        if stk and stk[-1] >= arr[i]:
             stk.pop()
         else:
             stk.append(arr[i])
