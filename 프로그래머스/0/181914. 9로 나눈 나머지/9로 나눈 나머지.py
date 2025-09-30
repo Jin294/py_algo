b7eb76573aec7cf02c33b25d6473058658fe1c7c
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2025-09-30 13:33:59 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 19.96 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.py
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.py
@@ -1,7 +1,5 @@
 def solution(number):
-    num_str = str(number)
     num_list = []
-    for x in num_str:
-        target_num = int(x)
-        num_list.append(target_num)
+    for x in number:
+        num_list.append(int(x))
     return sum(num_list) % 9
