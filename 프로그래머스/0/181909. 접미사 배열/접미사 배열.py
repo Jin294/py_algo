6e44167eec855532b9cdfed1595a6b99a3dc261f
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2025-09-30 15:11:54 +0900

    [level 0] Title: 접미사 배열, Time: 0.05 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,6 +1,2 @@
 def solution(my_string):
-    answer = []
-    for idx in range(len(my_string)):
-        answer.append(my_string[idx:])
-    answer.sort()
-    return answer
+    return sorted([my_string[idx:] for idx in range(len(my_string))])
